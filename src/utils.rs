048cf8cf56e702695e03cbd1e42b62b4284609a9
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-11 15:51:21 +0100

    Split os implementations into different mods

--- src/utils.rs
+++ src/utils.rs
@@ -1,28 +1,47 @@
-#![allow(unused_imports)] // TODO: Remove this line
-use std::process::Command;
-use std::{io, path::Path};
+#[cfg(target_os = "windows")]
+pub use self::windows::*;
+
+#[cfg(target_os = "macos")]
+pub use self::macos::*;
+
+#[cfg(target_os = "linux")]
+pub use self::linux::*;
 
 #[cfg(target_os = "windows")]
-pub fn open_file_in_folder(path: impl AsRef<Path>) -> Result<(), io::Error> {
-    Command::new("explorer.exe")
-        .arg("/select,")
-        .arg(path.as_ref())
-        .output()?;
-    Ok(())
+mod windows {
+    use std::process::Command;
+    use std::{io, path::Path};
+
+    pub fn open_file_in_folder(path: impl AsRef<Path>) -> Result<(), io::Error> {
+        Command::new("explorer.exe")
+            .arg("/select,")
+            .arg(path.as_ref())
+            .output()?;
+        Ok(())
+    }
 }
 
 #[cfg(target_os = "macos")]
-pub fn open_file_in_folder(path: impl AsRef<Path>) -> Result<(), io::Error> {
-    Command::new("open")
-        .arg("-R")
-        .arg("--")
-        .arg(path.as_ref())
-        .output()?;
-    Ok(())
+mod macos {
+    use std::process::Command;
+    use std::{io, path::Path};
+
+    pub fn open_file_in_folder(path: impl AsRef<Path>) -> Result<(), io::Error> {
+        Command::new("open")
+            .arg("-R")
+            .arg("--")
+            .arg(path.as_ref())
+            .output()?;
+        Ok(())
+    }
 }
 
 #[cfg(target_os = "linux")]
-pub fn open_file_in_folder(_path: impl AsRef<Path>) -> Result<(), io::Error> {
-    // TODO: Support linux: https://gitlab.gnome.org/World/pika-backup/-/blob/main/src/ui/page_archives/display.rs#L63
-    unimplemented!()
+mod linux {
+    use std::{io, path::Path};
+
+    pub fn open_file_in_folder(_path: impl AsRef<Path>) -> Result<(), io::Error> {
+        // TODO: Support linux: https://gitlab.gnome.org/World/pika-backup/-/blob/main/src/ui/page_archives/display.rs#L63
+        unimplemented!()
+    }
 }

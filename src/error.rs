c68ef47af63b387f267cf403471c97020392c44f
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-12 16:49:33 +0100

    Specialize transfer rejection error

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,28 @@
+use magic_wormhole::transfer::TransferError;
 use magic_wormhole::WormholeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-#[error(transparent)]
 pub enum PortalError {
+    #[error(transparent)]
     Wormhole(#[from] WormholeError),
-    WormholeTransfer(#[from] magic_wormhole::transfer::TransferError),
+    #[error(transparent)]
+    WormholeTransfer(TransferError),
+    #[error("Transfer rejected by peer")]
+    TransferRejected(TransferError),
+    #[error(transparent)]
     Io(#[from] std::io::Error),
+}
+
+const TRANSFER_REJECTED_MESSAGE: &str = "transfer rejected";
+
+impl From<TransferError> for PortalError {
+    fn from(value: TransferError) -> Self {
+        if let TransferError::PeerError(ref message) = value && message == TRANSFER_REJECTED_MESSAGE {
+            PortalError::TransferRejected(value)
+        }
+        else {
+            PortalError::WormholeTransfer(value)
+        }
+    }
 }

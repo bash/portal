5bf123d9bdc5a2e2aea387485d0afae3a82d5e2f
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-03-04 23:10:46 +0100

    Fix installer icon

--- build.rs
+++ build.rs
@@ -7,6 +7,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 #[cfg(windows)]
 fn build_windows() -> Result<(), Box<dyn Error>> {
+    use std::fs;
+    use std::path::PathBuf;
+
     use ico_builder::IcoBuilder;
     use winresource::WindowsResource;
 
@@ -14,6 +17,11 @@ fn build_windows() -> Result<(), Box<dyn Error>> {
         .add_source_file("build/windows/icon-32x32.png")
         .add_source_file("build/windows/icon-256x256.png")
         .build_file_cargo("windows-app-icon.ico")?;
+
+    let mut ico_file_path = PathBuf::from("target");
+    ico_file_path.push("icon.ico");
+    fs::copy(&icon, ico_file_path)?;
+
     let mut res = WindowsResource::new();
     res.set_icon(icon.to_str().unwrap());
     res.compile()?;

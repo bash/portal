7d166a9f27b3565252d26a25ddaaba1e2d7acfb0
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-03-04 09:44:44 +0100

    Generate windows icon on build

--- build.rs
+++ build.rs
@@ -1,4 +1,26 @@
-fn main() {
+use std::error::Error;
+use windows_app_icon::cargo::build_ico_file;
+use windows_app_icon::IconSizes;
+use winresource::WindowsResource;
+
+fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(windows)]
-    embed_resource::compile("build/windows/icon.rc");
+    build_windows()?;
+    Ok(())
+}
+
+#[cfg(windows)]
+fn build_windows() -> Result<(), Box<dyn Error>> {
+    let icon = build_ico_file(
+        "windows-app-icon.ico",
+        [
+            "build/windows/icon-32x32.png",
+            "build/windows/icon-1920x1920.png",
+        ],
+        IconSizes::MINIMAL,
+    )?;
+    let mut res = WindowsResource::new();
+    res.set_icon(icon.to_str().unwrap());
+    res.compile()?;
+    Ok(())
 }

a2c42200e7a716a1a4c23b7de063f6c103ebdd94
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-03-04 22:42:09 +0100

    Update ico-builder

--- build.rs
+++ build.rs
@@ -11,8 +11,8 @@ fn build_windows() -> Result<(), Box<dyn Error>> {
     use winresource::WindowsResource;
 
     let icon = IcoBuilder::default()
-        .source_file("build/windows/icon-32x32.png")
-        .source_file("build/windows/icon-256x256.png")
+        .add_source_file("build/windows/icon-32x32.png")
+        .add_source_file("build/windows/icon-256x256.png")
         .build_file_cargo("windows-app-icon.ico")?;
     let mut res = WindowsResource::new();
     res.set_icon(icon.to_str().unwrap());

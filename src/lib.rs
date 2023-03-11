346351546574d6653207a85fcf992011866a1595
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-03-11 22:49:40 +0100

    Add link to repository

--- src/lib.rs
+++ src/lib.rs
@@ -87,10 +87,20 @@ fn show_version(ctx: &egui::Context) {
         .show_separator_line(false)
         .show(ctx, |ui| {
             ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                ui.visuals_mut().hyperlink_color = ui.visuals().weak_text_color();
+
+                const REPOSITORY_URL: &str = "https://github.com/bash/portal";
+
                 #[cfg(debug_assertions)]
-                ui.weak("dev");
+                ui.hyperlink_to("dev", REPOSITORY_URL);
                 #[cfg(not(debug_assertions))]
-                ui.weak(env!("CARGO_PKG_VERSION"));
+                ui.hyperlink_to(
+                    env!("CARGO_PKG_VERSION"),
+                    format!(
+                        "{REPOSITORY_URL}/releases/tag/v{}",
+                        env!("CARGO_PKG_VERSION")
+                    ),
+                );
             })
         });
 }

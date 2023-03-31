5f1d9350f539c7b3ce82d506424eaec22f3ccf92
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-03-31 22:11:10 +0200

    Add tests for conflict resolution

--- crates/portal-wormhole/src/fs/persist.rs
+++ crates/portal-wormhole/src/fs/persist.rs
@@ -26,10 +26,11 @@ fn to_persist_result<T>(result: Result<T, tempfile::PersistError>) -> PersistRes
 
 pub fn persist_with_conflict_resolution<T>(
     state: T,
-    path: PathBuf,
+    mut path: PathBuf,
     filename: Filename,
     save: impl FnMut(T, &Path) -> PersistResult<T>,
 ) -> io::Result<PathBuf> {
+    path.push(filename.to_os_string());
     return persist(state, path, filename, save, 0);
 
     #[tailcall]
@@ -51,6 +52,64 @@ pub fn persist_with_conflict_resolution<T>(
             ),
             PersistResult::Ok => Ok(path),
             PersistResult::Err(error) => Err(error),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::fs::sanitize_untrusted_filename;
+    use std::collections::VecDeque;
+
+    #[test]
+    fn uses_original_file_name_when_possible() {
+        let expected_path = PathBuf::from("bar/foo.txt");
+        let filename = filename_from_path("foo.txt".as_ref());
+        let path = persist_with_conflict_resolution(
+            VecDeque::default(),
+            PathBuf::from("bar"),
+            filename,
+            save,
+        )
+        .unwrap();
+        assert_eq!(path, expected_path);
+    }
+
+    #[test]
+    fn retries_on_conflict() {
+        let existing_paths = vec![
+            PathBuf::from("bar/foo.txt"),
+            PathBuf::from("bar/foo (1).txt"),
+            PathBuf::from("bar/foo (2).txt"),
+            PathBuf::from("bar/foo (3).txt"),
+        ];
+        let expected_path = PathBuf::from("bar/foo (4).txt");
+        let filename = filename_from_path("foo.txt".as_ref());
+        let path = persist_with_conflict_resolution(
+            existing_paths.into(),
+            PathBuf::from("bar"),
+            filename,
+            save,
+        )
+        .unwrap();
+        assert_eq!(path, expected_path);
+    }
+
+    fn filename_from_path(path: &Path) -> Filename<'_> {
+        sanitize_untrusted_filename(path, "fallback_stem".as_ref(), "fallback_ext".as_ref())
+    }
+
+    fn save(
+        mut existing_paths: VecDeque<PathBuf>,
+        path: &Path,
+    ) -> PersistResult<VecDeque<PathBuf>> {
+        match existing_paths.pop_front() {
+            None => PersistResult::Ok,
+            Some(expected_path) => {
+                assert_eq!(path, expected_path);
+                PersistResult::Conflict(existing_paths)
+            }
         }
     }
 }
